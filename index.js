4266ff6ae8452c5ffbf31c5df3ad23b0a6703bfd
Dan Souza <Souza.music@outlook.com>
2022-02-04 15:42:17 -0800

    assignment completed

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function (divide = 2000 / 100) {
+    console.log(divide)
+    return divide    
+}
+
+const square = parameter1 => parameter1 * parameter1;
+
+const add = (a, b) => a + b;
